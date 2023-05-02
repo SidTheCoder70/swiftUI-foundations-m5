44d9354f667620acf9f8972f1b53a0e5a046f793
mudguts70 <e_ruston@hotmail.com>
2023-05-02 16:58:31 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Eric Ruston on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
